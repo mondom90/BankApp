21f120b54191d9579ca86eb7d1e51527acbae02d
MandS <monika.domaradzkaa@gmail.com>
2022-12-05 17:26:06 +0000

    Dodanie nowej klasy User

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Witaj w naszym banku!");
+        System.out.println("Wprowadz imie");
+        Scanner scanner=new Scanner(System.in);
+        String name= scanner.nextLine();
+        System.out.println("Wprowadz nazwisko");
+        String surname= scanner.nextLine();
+        System.out.println("Wprowadz numer pesel");
+        int pesel= scanner.nextInt();
+
+        System.out.println("Menu\n 1.Informacja o koncie\n 2.Wplata pieniedzy\n 3.Wyplata pieniedzy\n 4.Wyjscie" );
+
+    }
+
+
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class User {
+    private String name;
+    private String surname;
+    private String nickname;
+    private int pesel;
+    private int accountNumber;
+    private double balance=100;
+
+    public User(String name, String surname, int pesel){
+        this.name=name;
+        this.surname=surname;
+        this.pesel=pesel;
+
+
+
+
+    }
+}
