6d47f61bfc5a56148e0e2ad97049db9b293347ab
MandS <monika.domaradzkaa@gmail.com>
2022-12-05 17:59:30 +0000

    Dodanie uzytkownika 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w naszym banku!");
         System.out.println("Wprowadz imie");
-        Scanner scanner=new Scanner(System.in);
-        String name= scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         System.out.println("Wprowadz nazwisko");
-        String surname= scanner.nextLine();
+        String surname = scanner.nextLine();
         System.out.println("Wprowadz numer pesel");
-        int pesel= scanner.nextInt();
+        int pesel = scanner.nextInt();
+
+        User user1=new User(name, surname, pesel);
 
         System.out.println("Menu\n 1.Informacja o koncie\n 2.Wplata pieniedzy\n 3.Wyplata pieniedzy\n 4.Wyjscie" );
 
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -6,15 +6,18 @@ public class User {
     private String nickname;
     private int pesel;
     private int accountNumber;
-    private double balance=100;
+    private double balance;
 
     public User(String name, String surname, int pesel){
         this.name=name;
         this.surname=surname;
         this.pesel=pesel;
-
-
-
-
+        this.nickname=nicknameGenerator(name,surname);
+        this.balance=100;
+        this.accountNumber=1;
     }
+    public String nicknameGenerator(String name, String surname) {
+        return (name.substring(0,3)+surname.substring(0,3));
+    }
+
 }
